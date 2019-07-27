8745bf188fe2859b3e030842ef08929dad26cfa9
danielFrancisOlivieri <daniel@outdoorjournal.com>
2019-07-27 17:55:43 -0400

    adding html

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var http = require("http");
+var fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('The truth about the world, he said, is that anything is possible.');
-})
 
-app.listen(3000, function () {
-  console.log('We will take upon ourselves the mystery of things.')
-})
+http.createServer(function(req, res) {
+
+  fs.readFile("index.html", function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+
+}).listen(3000);
