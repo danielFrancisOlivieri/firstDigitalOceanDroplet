7334cf4bb6cc17a0d830eaef882c4c0745d320ca
danielFrancisOlivieri <daniel@outdoorjournal.com>
2019-07-27 17:40:48 -0400

    Daniel-ifying it

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Digital Ocean!')
+  res.send('The truth about the world, he said, is that anything is possible.');
 })
 
 app.listen(3000, function () {
-  console.log('Magic is happening on port 3000!')
+  console.log('We will take upon ourselves the mystery of things.')
 })
